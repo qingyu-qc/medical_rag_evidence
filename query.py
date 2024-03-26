5201db1a06411ed18481e5be0839fece21091f9c
XuguangAi <161768196+XuguangAiYale@users.noreply.github.com>
2024-03-26 17:28:57 -0400

    Update query.py

--- query.py
+++ query.py
@@ -1,7 +1,18 @@
 ##################################### Import libraries #####################################
 import os
+import argparse
 from llama_index.core import Document, StorageContext, load_index_from_storage, SimpleDirectoryReader, VectorStoreIndex
 import openai
+
+##################################### Parse arguments #####################################
+
+parser = argparse.ArgumentParser(description='Query with custom parameters.')
+parser.add_argument('--similarity_top_k', type=int, default=10, help='Number of selected references')
+parser.add_argument('--question', type=str, required=True, help='The question to query')
+args = parser.parse_args()
+
+similarity_top_k = args.similarity_top_k
+question = args.question 
 
 ##################################### Set up path, key and var #####################################
 
@@ -31,7 +42,6 @@ index = load_index_from_storage(storage_context)
 full_query_engine = index.as_query_engine(similarity_top_k=similarity_top_k)
 
 # query an example question
-question = 'your eye related question'
 query_answer = query_llama(
     'Answer this paitients question and provide references at the end of your responses. The references should follow the AMA format: \n',
     question)
